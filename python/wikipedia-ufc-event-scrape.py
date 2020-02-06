9043ed05a9d1a34c1ae60054f311e484816353b8
Mark Pereira <me@markpereira.com>
2020-02-06 10:32:58 -0500

    Update wikipedia-ufc-event-scrape.py

--- python/wikipedia-ufc-event-scrape.py
+++ python/wikipedia-ufc-event-scrape.py
@@ -193,7 +193,7 @@ def insertRows (row_len, prev_row_ptr, array_pos):
     return;
 
 # Database Connection
-db = MySQLdb.connect(host="markpereira.com", user="mark5463_ft_test", passwd="fttesting", db="mark5463_ft_prod", charset="utf8", encoding="utf-8")
+db = MySQLdb.connect(host="markpereira.com", user="mark5463_ft_test", passwd="fttesting", db="mark5463_ft_prod", charset="utf8")
 
 # Cursor object. It will let you execute the queries
 cur = db.cursor()

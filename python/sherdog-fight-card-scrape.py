7d0f4298ceb8cbc7adcc948b875aba2ac324c8bb
Mark Pereira <me@markpereira.com>
2021-04-26 15:03:19 -0400

    typo

--- python/sherdog-fight-card-scrape.py
+++ python/sherdog-fight-card-scrape.py
@@ -33,10 +33,11 @@ def scrapeEvent(event_url, event_org):
 
 
 # Database Connection
-db = MySQLdb.connect(host="markpereira.com",  # your host, usually localhost
-                     user="mark5463_ft_test",  # your username
-                     passwd="fttesting",  # your password
-                     db="mark5463_ft_prod")  # name of the data base
+# db = MySQLdb.connect(host="markpereira.com",  # your host, usually localhost
+#                     user="mark5463_ft_test",  # your username
+#                     passwd="fttesting",  # your password
+#                     db="mark5463_ft_prod")  # name of the data base
+db = MySQLdb.connect(host="135.23.254.253:3308", user="root", passwd="fttesting", db="mark5463_ft_prod")
 
 #  you must create a Cursor object. It will let
 #  you execute all the queries you need
@@ -209,7 +210,7 @@ fighterloop = len(g_fighter_one)
 #                     db="mark5463_ft_prod")
                      # db="mark5463_ft_testdb")  # name of the data base
     
-db = MySQLdb.connect(host="135.23.253.254:3308", user="root", passwd="fttesting", db="mark5463_ft_prod")
+db = MySQLdb.connect(host="135.23.254.253:3308", user="root", passwd="fttesting", db="mark5463_ft_prod")
 
 #  you must create a Cursor object. It will let
 #  you execute all the queries you need

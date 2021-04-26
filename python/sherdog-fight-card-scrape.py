acef056c843a590f2d4b468b8fcb0cd890371eb9
Mark Pereira <me@markpereira.com>
2021-04-26 14:55:33 -0400

    local mysql

--- python/sherdog-fight-card-scrape.py
+++ python/sherdog-fight-card-scrape.py
@@ -203,11 +203,13 @@ for x in range(0, x_range - 1):  # prev 0, 533
 
 fighterloop = len(g_fighter_one)
 
-db = MySQLdb.connect(host="markpereira.com",  # your host, usually localhost
-                     user="mark5463_ft_test",  # your username
-                     passwd="fttesting",  # your password
-                     db="mark5463_ft_prod")
+#db = MySQLdb.connect(host="markpereira.com",  # your host, usually localhost
+#                     user="mark5463_ft_test",  # your username
+#                     passwd="fttesting",  # your password
+#                     db="mark5463_ft_prod")
                      # db="mark5463_ft_testdb")  # name of the data base
+    
+db = MySQLdb.connect(host="135.23.253.254:3308", user="root", passwd="fttesting", db="mark5463_ft_prod")
 
 #  you must create a Cursor object. It will let
 #  you execute all the queries you need

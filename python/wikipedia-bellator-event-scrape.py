43b2fdf9f9fdbb560daf914b0f923506836dce30
futurepr0n <mark@futurepr0n.com>
2021-05-26 09:56:52 -0400

    addded printing stuff for debuggin

--- python/wikipedia-bellator-event-scrape.py
+++ python/wikipedia-bellator-event-scrape.py
@@ -58,7 +58,6 @@ def loadEventsData (event_url, event_org):
         event_fight_card_url_array = tree.xpath('//*[@id="mw-content-text"]/div/table/tbody/tr[%i]/td[2]/i/a/@href'%(x))
         new_fight_card_url_str = ''.join(event_fight_card_url_array)
         ascii_fight_card_url_string = smart_str(new_fight_card_url_str)
-
         if ascii_event_name_string == '': # Try the td 1 flavor
           event_name_array = tree.xpath('//*[@id="mw-content-text"]/div/table/tbody/tr[%i]/td[1]/a/text()'%(x))
           new_event_name_str = ''.join(event_name_array)
@@ -67,7 +66,6 @@ def loadEventsData (event_url, event_org):
           event_fight_card_url_array = tree.xpath('//*[@id="mw-content-text"]/div/table/tbody/tr[%i]/td[1]/a/@href'%(x))
           new_fight_card_url_str = ''.join(event_fight_card_url_array)
           ascii_fight_card_url_string = smart_str(new_fight_card_url_str)
-
           if ascii_event_name_string == '': # Try the td 1 flavor with italic
             event_name_array = tree.xpath('//*[@id="mw-content-text"]/div/table/tbody/tr[%i]/td[1]/i/a/text()'%(x))
             new_event_name_str = ''.join(event_name_array)
@@ -79,17 +77,14 @@ def loadEventsData (event_url, event_org):
             event_name.append(ascii_event_name_string)
             ev_fc_wbst = 'http://en.wikipedia.org', ''.join(ascii_fight_card_url_string)
             event_fight_card_url.append(ev_fc_wbst)
-
           else:
             event_name.append(ascii_event_name_string)
-
             ev_fc_wbst = 'http://en.wikipedia.org', ''.join(ascii_fight_card_url_string)
             event_fight_card_url.append(ev_fc_wbst)
-          
+        
           event_name.append(ascii_event_name_string)
           ev_fc_wbst = 'http://en.wikipedia.org', ''.join(ascii_fight_card_url_string)
           event_fight_card_url.append(ev_fc_wbst)
-          
         else: 
           event_name.append(ascii_event_name_string)
           ev_fc_wbst = 'http://en.wikipedia.org', ''.join(ascii_fight_card_url_string)
@@ -113,12 +108,13 @@ def loadEventsData (event_url, event_org):
         event_date_array = tree.xpath('//*[@id="mw-content-text"]/div/table/tbody/tr[%i]/td[2]/span/text()'%(x))
         new_event_date = ''.join(event_date_array)
         ascii_event_date = smart_str(new_event_date)
-
         event_date.append(ascii_event_date)
       else:                      
-                                     
         event_date.append(ascii_event_date)
 
+      print('Adding event: %s ...' % ascii_event_name_string)
+      print('Event Date: \t', ascii_event_date)
+      print('Event URL: \t\t %s' % ascii_fight_card_url_string)
 
     return row_len;
 

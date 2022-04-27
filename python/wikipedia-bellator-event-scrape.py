7bed08de69ea046da328c6ca35e0fcb4f8924adc
Mark Pereira <me@markpereira.com>
2022-04-27 16:50:12 -0400

    Update wikipedia-bellator-event-scrape.py

--- python/wikipedia-bellator-event-scrape.py
+++ python/wikipedia-bellator-event-scrape.py
@@ -156,7 +156,8 @@ def insertRows (row_len, prev_row_ptr, array_pos):
 
 # Database Connection
 #db = MySQLdb.connect(host="markpereira.com", user="mark5463_ft_test", passwd="fttesting", db="mark5463_ft_prod")
-db = MySQLdb.connect(host="dev-mysql.markpereira.com", user="root", passwd="fttesting", db="mark5463_ft_prod")
+#db = MySQLdb.connect(host="dev-mysql.markpereira.com", user="root", passwd="fttesting", db="mark5463_ft_prod")
+db = MySQLdb.connect(user="root", passwd="fttesting", port=3308, db="mark5463_ft_prod")
 
 
 # Cursor object. It will let you execute the queries

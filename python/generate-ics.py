d419198b72ba1d3dc34dccd036b01ab86a42cc7a
Mark Pereira <me@markpereira.com>
2023-02-25 17:57:23 -0500

    fixed, not elegantly, but works

--- python/generate-ics.py
+++ python/generate-ics.py
@@ -13,18 +13,18 @@ import MySQLdb
 # mw-content-text > table:nth-child(52) > tbody > tr:nth-child(2) > td
 def month_converter(month):
     return {
-        'Jan': '01',
-        'Feb': '02',
-        'Mar': '03',
-        'Apr': '04',
-        'May': '05',
-        'Jun': '06',
-        'Jul': '07',
-        'Aug': '08',
-        'Sep': '09',
-        'Oct': '10',
-        'Nov': '11',
-        'Dec': '12'
+        '1': '01',
+        '2': '02',
+        '3': '03',
+        '4': '04',
+        '5': '05',
+        '6': '06',
+        '7': '07',
+        '8': '08',
+        '9': '09',
+        '10': '10',
+        '11': '11',
+        '12': '12'
     }[month]
 
 

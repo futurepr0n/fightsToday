6b4d71a7ddcb8044f5e3ed9a38957e844fc59165
Mark Pereira <me@markpereira.com>
2023-02-25 19:48:57 -0500

    trying with casting, last attempt failed

--- python/generate-ics.py
+++ python/generate-ics.py
@@ -130,9 +130,9 @@ def createEvents(event_date, event_fight_card_url, event_name, event_org):
     print(dt.day)
     print(dt.year)
     
-    event_month = dt.month
-    event_day = dt.day
-    event_year = dt.year
+    event_month = str(dt.month)
+    event_day = str(dt.day)
+    event_year = str(dt.year)
 
 
     newMonth = month_converter(event_month)

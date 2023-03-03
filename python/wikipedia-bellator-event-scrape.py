91b5eee7304f9309315cf44120755468f85d6f59
Mark Pereira <me@markpereira.com>
2023-03-03 13:52:25 -0500

    had to add logic

--- python/wikipedia-bellator-event-scrape.py
+++ python/wikipedia-bellator-event-scrape.py
@@ -187,7 +187,7 @@ def insertRows (row_len, prev_row_ptr, array_pos):
       print("d1 is greater than today: ", d1 > d2)
       print("d1 is less than d2 : ", d1 < d2)
       print("d1 is not equal to d2 : ", d1 != d2)
-      if compare_date_1 > compare_date_today
+      if compare_date_1 > compare_date_today == true
         bellator_se_num = bellator_se_num + 1
       else
         bellator_pe_num = bellator_pe_num + 1

52d84edd2e0ef156376c3aecb88d8d129ff7af37
Mark Pereira <me@markpereira.com>
2023-04-12 17:13:34 -0400

    Changed to php

--- python/upload-files.py
+++ python/upload-files.py
@@ -1,7 +1,7 @@
 import ftplib
 session = ftplib.FTP('ftp.markpereira.com','fights@markpereira.com','fights2dayftp')
-file = open('index.html','rb')                  # file to send
-session.storbinary('STOR index.html', file)     # send the file
+file = open('index.php','rb')                  # file to send
+session.storbinary('STOR index.php', file)     # send the file
 file.close()                                    # close file and FTP
 file = open('all_events.ics','rb')                  # file to send
 session.storbinary('STOR all_events.ics', file)     # send the file

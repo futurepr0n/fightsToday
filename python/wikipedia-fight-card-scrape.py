226be2571b2540eb5af4667c2184cdf3114e7d25
Mark Pereira <me@markpereira.com>
2023-05-04 12:16:52 -0400

    Update wikipedia-fight-card-scrape.py

--- python/wikipedia-fight-card-scrape.py
+++ python/wikipedia-fight-card-scrape.py
@@ -117,7 +117,7 @@ for x in range(0, x_range - 1):  # prev 0, 533
         g_fight_card_org.append(this_event_org)
         g_fight_card_wiki_event_id.append(this_wiki_event_id)
         g_fight_card_event_past.append(str(this_event_past))
-        e_name = ''.join(g_fight_card_event_name[y])
+        e_name = ''.join(this_event_name)
         e_f1 = ''.join(asccii_string3)
         e_f1_url = ''.join(fgtr1_wbst)
         e_f2 = ''.join(asccii_string4)

44621b16360790cadf63b8da610387ba70c2fd2e
Mark Pereira <me@markpereira.com>
2023-05-04 21:31:30 -0400

    Update wikipedia-fight-card-scrape.py

--- python/wikipedia-fight-card-scrape.py
+++ python/wikipedia-fight-card-scrape.py
@@ -172,11 +172,11 @@ for x in range(0, x_range - 1):  # prev 0, 533
         newstr4 = ''.join(fighter_two_array)
         asccii_string4 = smart_str(newstr4)
         if asccii_string4 == '':
-            fighter_one_array = tree.xpath('//*[@id="mw-content-text"]/div[1]/table[2]/tbody/tr[%i]/td[4]/text()' % (z))
+            fighter_two_array = tree.xpath('//*[@id="mw-content-text"]/div[1]/table[2]/tbody/tr[%i]/td[4]/text()' % (z))
             newstr4 = ''.join(fighter_two_array) 
             asccii_string4 = smart_str(newstr4)
             if asccii_string4 == '':
-                fighter_one_array = tree.xpath('//*[@id="mw-content-text"]/div[1]/table[3]/tbody/tr[%i]/td[4]/text()' % (z))
+                fighter_two_array = tree.xpath('//*[@id="mw-content-text"]/div[1]/table[3]/tbody/tr[%i]/td[4]/text()' % (z))
                 newstr4 = ''.join(fighter_two_array) 
                 asccii_string4 = smart_str(newstr4)
                 g_fighter_two.append(asccii_string4)

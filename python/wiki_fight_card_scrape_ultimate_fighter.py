325a3e2b61d87d01e7ca01e7e615562b03ab6812
Mark Pereira <me@markpereira.com>
2023-05-06 23:03:18 -0400

    Update wiki_fight_card_scrape_ultimate_fighter.py

--- python/wiki_fight_card_scrape_ultimate_fighter.py
+++ python/wiki_fight_card_scrape_ultimate_fighter.py
@@ -96,6 +96,15 @@ for x in range(0, x_range - 1):  # prev 0, 533
     row_len = len(p) + 1
      
     for z in range(3, row_len):
+        asccii_string3 = ''
+        fgtr1_wbst = ''
+        asccii_string4 = ''
+        fgtr2_wbst = ''
+        ascii_fight_weightclass = ''
+        ascii_fight_method = ''
+        ascii_fight_time = ''
+        ascii_fight_round = ''
+        
         fighter_one_array = None
         for xpath in ['//*[@id="mw-content-text"]/div[1]/table[7]/tbody/tr[%i]/td[2]/a/text()',
                       '//*[@id="mw-content-text"]/div[1]/table[7]/tbody/tr[%i]/td[2]/text()',

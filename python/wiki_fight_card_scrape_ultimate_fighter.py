2e9f5baa9a6e207a02402a0aff608cbb78233147
Mark Pereira <me@markpereira.com>
2023-05-07 01:01:35 -0400

    Update wiki_fight_card_scrape_ultimate_fighter.py

--- python/wiki_fight_card_scrape_ultimate_fighter.py
+++ python/wiki_fight_card_scrape_ultimate_fighter.py
@@ -265,6 +265,6 @@ for x in range(0, x_range - 1):  # prev 0, 533
             cur.execute(query)
         else:
             print("Not all required variables have a value. Skipping database insertion.")
-            print(e_name,e_f1,ef_2,e_fc_url)
+            print(e_name,e_f1,e_f2,e_fc_url)
 
 

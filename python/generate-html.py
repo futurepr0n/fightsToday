301afb4b627cf7070404a0c97a7acc6ecb9844ec
futurepr0n <mark@futurepr0n.com>
2023-05-19 11:46:45 -0400

    more updates for seo

--- python/generate-html.py
+++ python/generate-html.py
@@ -82,7 +82,7 @@ session_start();
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="Fights Today upcoming & past events. Bellator & UFC Event previews. Stats, trivia, ical subscription Calendar. Log in track your stats & compete."/>
+        <meta name="description" content="Fights Today upcoming & past events. Bellator & UFC Event previews. Stats, Rankings, MMA Trivia, iCal subscription Calendar. Log in track your stats & compete."/>
         <meta name="author" content="" />
         <title>fights.Today - Upcoming UFC Bellator MMA Events; Make Predictions, Play Trivia. Subscribe to our iCal</title>
         <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />

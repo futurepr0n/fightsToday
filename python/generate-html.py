5ea30ad48ee3e0e0dd3a249e7ebc5da9b5fe3d68
futurepr0n <mark@futurepr0n.com>
2023-05-24 09:44:13 -0400

    modifying the desc and title

--- python/generate-html.py
+++ python/generate-html.py
@@ -82,9 +82,9 @@ session_start();
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="Fights Today upcoming & past events. Bellator & UFC Event previews. Stats, Rankings, MMA Trivia, iCal subscription Calendar. Log in track your stats & compete."/>
+        <meta name="description" content="Fights Today upcoming & past events. Bellator & UFC Event previews. Stats, Rankings, MMA Trivia, iCal subscription. Track your stats & compete."/>
         <meta name="author" content="" />
-        <title>fights.Today - Upcoming UFC Bellator MMA Events; Make Predictions, Play Trivia. Subscribe to our iCal</title>
+        <title>Welcome to fights.Today - Your source for Upcoming UFC Bellator MMA Events</title>
         <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>

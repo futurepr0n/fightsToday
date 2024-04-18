28881c490c568611448152ce738e7e1c584d5dd6
futurepr0n <mark@futurepr0n.com>
2024-04-18 18:49:35 -0400

    added some try stuff in case it helps with the url that wont load

--- python/wikipedia-pfl-poster-scrape.py
+++ python/wikipedia-pfl-poster-scrape.py
@@ -20,25 +20,34 @@ import os
 #mw-content-text > table:nth-child(52) > tbody > tr:nth-child(2) > td
 
 def loadPosterData (event_url):
-    #set up the lxml, load url to scrape
-    page = requests.get('%s'%(event_url))
-    tree = html.fromstring(page.content)
+    try:
+        # Set up the lxml, load URL to scrape
+        page = requests.get(event_url)
+        page.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+        tree = html.fromstring(page.content)
 
-    #set up PyQuery section, load the url to scrape
-    d = pq("<html></html>")
-    d = pq(etree.fromstring("<html></html>"))
-    d = pq(url='%s'%(event_url))
+        # Your existing code for processing the page and extracting the poster URL...
+        #set up PyQuery section, load the url to scrape
+        d = pq("<html></html>")
+        d = pq(etree.fromstring("<html></html>"))
+        d = pq(url='%s'%(event_url))
 
-    poster_url_array = tree.xpath('//*[@id="mw-content-text"]/div[1]/table[1]/tbody/tr[2]/td/span/a/img/@src')
+        poster_url_array = tree.xpath('//*[@id="mw-content-text"]/div[1]/table[1]/tbody/tr[2]/td/span/a/img/@src')
 
-    # If the poster is not found, we might want to try this xpath: //*[@id="mw-content-text"]/table[5]/tr[2]/td/a/img
+        # If the poster is not found, we might want to try this xpath: //*[@id="mw-content-text"]/table[5]/tr[2]/td/a/img
 
 
-    ev_fc_poster_wbst = str(poster_url_array).strip('[\'\']')
-    newstr = ev_fc_poster_wbst
-    ev_fp_wbst = "https:%s"%(newstr)
+        ev_fc_poster_wbst = str(poster_url_array).strip('[\'\']')
+        newstr = ev_fc_poster_wbst
+        ev_fp_wbst = "https:%s"%(newstr)
 
-    return ev_fp_wbst;
+        return ev_fp_wbst
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while fetching the URL {event_url}: {e}")
+        # Return a default URL
+        ev_fp_wbst = "https://fights.today/pfl_placeholder.png"
+        return ev_fp_wbst
+        
 
 def insertRows (poster_url, event_id, event_fight_card_url, event_date, event_name, event_org):
     # print('+++++++++++++++++++++++++++++')

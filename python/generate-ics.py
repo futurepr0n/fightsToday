8f8e61fd0057737a565da71048cf782c56a9affd
futurepr0n <mark@futurepr0n.com>
2025-07-16 13:09:10 -0400

    fix to the ics

--- python/generate-ics.py
+++ python/generate-ics.py
@@ -2,6 +2,8 @@ from lxml import html, etree
 from ics import Calendar, Event
 import io
 import datetime
+import calendar
+import logging
 #from django.utils.encoding import smart_str, smart_text
 import django
 from django.utils.encoding import smart_str
@@ -11,6 +13,15 @@ import requests
 import MySQLdb
 import os
 
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(),  # Output to console
+        logging.FileHandler('ics_generation.log')  # Output to file
+    ]
+)
 
 ##### The JQuery for "The Ultimate Fighter" Posters is:
 
@@ -120,6 +131,48 @@ def month_end_converter(day,mth):
     return endmth;
 
 
+def parse_event_date(event_date):
+    """
+    Parse event date string with support for various formats including incomplete dates.
+    
+    Args:
+        event_date (str): The date string to parse
+        
+    Returns:
+        datetime.datetime: Parsed date object, or None if parsing fails
+    """
+    if not event_date or event_date.strip() == "":
+        return None
+    
+    # Strip whitespace
+    event_date = event_date.strip()
+    
+    # List of date formats to try, in order of preference
+    date_formats = [
+        "%B %d, %Y",      # January 15, 2025
+        "%b %d, %Y",      # Jan 15, 2025
+        "%d %B %Y",       # 15 January 2025
+        "%B %Y",          # January 2025 (incomplete - will default to 1st)
+        "%b %Y",          # Jan 2025 (incomplete - will default to 1st)
+        "%Y-%m-%d",       # 2025-01-15
+        "%m/%d/%Y",       # 01/15/2025
+        "%d/%m/%Y",       # 15/01/2025
+    ]
+    
+    for date_format in date_formats:
+        try:
+            dt = datetime.datetime.strptime(event_date, date_format)
+            # For month-only formats, the day will be 1 by default
+            logging.info(f"Successfully parsed date '{event_date}' using format '{date_format}'")
+            return dt
+        except ValueError:
+            continue
+    
+    # If no format worked, log the error and return None
+    logging.warning(f"Unable to parse date: '{event_date}'. Skipping this event.")
+    return None
+
+
 def createEvents(event_date, event_fight_card_url, event_name, event_org):
     print('+++++++++++++++++++++++++++++')
     print('Event Date: ')
@@ -140,19 +193,13 @@ def createEvents(event_date, event_fight_card_url, event_name, event_org):
     e = Event()
     e.name = event_name
     
-    if event_date == "":
-        return
-
-    # Strip leading and trailing whitespace from event_date
-    event_date = event_date.strip()
-
+    # Parse the event date using our robust date parser
+    dt = parse_event_date(event_date)
     
-    try:
-        dt = datetime.datetime.strptime(event_date, "%B %d, %Y")
-    except ValueError:
-        dt = datetime.datetime.strptime(event_date, "%b %d, %Y")
-    except ValueError: 
-        dt = datetime.datetime.strptime(event_date, "%d %B %Y")
+    # If date parsing failed, skip this event
+    if dt is None:
+        logging.warning(f"Skipping event '{event_name}' due to unparseable date: '{event_date}'")
+        return
     
     print(dt.month)
     print(dt.day)
@@ -224,7 +271,8 @@ cur = db.cursor()
 
 
 # This section will query the database and return all data in the table
-cur.execute("SELECT event_name, event_id, event_fight_card_url, event_org, event_date, wiki_event_id FROM wiki_mma_events")
+# Use DISTINCT to prevent duplicate events in case of database issues
+cur.execute("SELECT DISTINCT event_name, event_id, event_fight_card_url, event_org, event_date, wiki_event_id FROM wiki_mma_events ORDER BY event_date, event_name")
 
 # initialize the arrays
 event_name = []
@@ -235,6 +283,9 @@ event_date = []
 wiki_event_id = []
 
 MMACalendar = Calendar()
+
+# Track processed events to prevent duplicates
+processed_events = set()
 
 # fight poster specific arrays
 # fight_card_poster = []
@@ -253,8 +304,18 @@ x_range = len(event_name)
 
 # This loops for every entry of event in the database to build our fight card information
 for x in range(0, x_range):  # prev 0, 533
+    # Create a unique identifier for this event to prevent duplicates
+    event_key = (event_name[x], event_org[x], event_date[x])
+    
+    # Skip if we've already processed this event
+    if event_key in processed_events:
+        logging.info(f"Skipping duplicate event: {event_name[x]} - {event_org[x]} - {event_date[x]}")
+        continue
+    
+    # Mark this event as processed
+    processed_events.add(event_key)
+    
     # bring in the url information
-
     createEvents(event_date[x], event_fight_card_url[x], event_name[x], event_org[x])
 
     # time.sleep(5)
